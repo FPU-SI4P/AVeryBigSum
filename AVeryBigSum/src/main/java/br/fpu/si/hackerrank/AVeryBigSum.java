3ce23c9fb03d34db486f1ec48150c10d1268a117
aluno <aluno@LAB02MAQ21.polilab.local>
2015-08-04 20:24:18 -0300

    Teste unitario para problema A Very Big Sum

---
+++ AVeryBigSum/src/main/java/br/fpu/si/hackerrank/AVeryBigSum.java
@@ -0,0 +1,7 @@
+package br.fpu.si.hackerrank;
+
+public class AVeryBigSum {
+	public Long bigSum(Long... entradas) {
+		return 0l;
+	}
+}
---
+++ AVeryBigSum/src/test/java/br/fpu/si/hackerrank/AVeryBigSumTest.java
@@ -0,0 +1,42 @@
+package br.fpu.si.hackerrank;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AVeryBigSumTest {
+
+	private AVeryBigSum aVeryBigSum;
+	
+	@Before
+	public void setUp(){
+		this.aVeryBigSum = new AVeryBigSum();
+	}
+	
+	@Test
+	public void somaVazia(){
+		assertThat(aVeryBigSum.bigSum(), is(0l));
+	}
+	
+	@Test
+	public void somaUnicoNumero(){
+		assertThat(aVeryBigSum.bigSum(7l), is(7l));
+	}
+	
+	@Test
+	public void somaDoisNumeros(){
+		assertThat(aVeryBigSum.bigSum(6l,5l), is(11l));
+	}
+	
+	@Test
+	public void somaTresNumeros(){
+		assertThat(aVeryBigSum.bigSum(6l,5l,9l), is(20l));
+	}
+	
+	@Test
+	public void somaComNumeroNegativo(){
+		assertThat(aVeryBigSum.bigSum(-6l,5l,9l), is(8l));
+	}
+}
